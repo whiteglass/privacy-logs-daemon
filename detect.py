0f606864db5ebf4c4d9004a3eabfd0cf8a7e76ed
glassSkyy <git.glutton080@passmail.com>
2026-06-01 19:29:14 +0100

    added pattern based detection for emails and passwords in logs

--- detect.py
+++ detect.py
@@ -1,5 +1,17 @@
 import re
 
+#patterns that are used to detect email and password
 email_regex = r"\b[A-Za-z0-9._%+_]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"
 password_regex = r"(?i)\b(password|pwd|passwd|secret|passphrase|pass)\b\s*[:=]\s*(\".*?\"|\S+)"
 
+def detection(data):
+    matches = []
+
+    for match in re.finditer(email_regex, data): 
+        matches.append(("email", match.group())) 
+
+    for match in re.finditer(password_regex, data):
+        matches.append(("password", match.group(2)))
+
+    #the code above looks for a match (using the pattern rules) , then adds it to the end of matches variable and returns it.
+    return matches
