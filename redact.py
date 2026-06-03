9c0ae819f90aa231446d8feebc13c309bfcf4e10
glass-skyy <git.glutton080@passmail.com>
2026-06-03 19:01:08 +0100

    added a mask, hash and redact mode

--- redact.py
+++ redact.py
@@ -1,1 +1,13 @@
 #three redaction modes will be here: mask using *, hash and full redaction
+
+import hashlib
+
+def mask(value):
+    return value[:3] + "*" * (len(value) - 3)
+
+def hash_value(value):
+    hashed = hashlib.sha256(value.encode()).hexdigest()
+    return f"[HASH:{hashed}]"
+
+def redact(value):
+    return "[REDACTED]"
