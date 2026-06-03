80a06f50a471a506bfc664a07e06f17fa4663cc0
glass-skyy <git.glutton080@passmail.com>
2026-06-03 20:13:33 +0100

    made config.py for loading config.json and includes a check to make sure the redaction mode that is set is valid

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
-#for config.json
+#this is used to load config.json
+
+import json
+import os
+
+config_path = os.path.join(os.path.dirname(__file__), "config.json")
+
+def load_config ():
+    with open(config_path, "r") as file:
+        config = json.load(file)
+    
+    redaction_modes = ["mask", "hash", "redact"]
+    if config["mode"] not in redaction_modes:
+        print(f"[ERROR]: invalid mode '{config['mode']}' returning to default mask mode")
+        config["mode"] = "mask"
+        
+    return config
